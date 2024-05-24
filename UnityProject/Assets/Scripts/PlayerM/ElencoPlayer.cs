19ed039d7a0b4094a83b714042bbe68a36802f72
PolloRequem <dantebaiunco29.06games@gmail.com>
2024-05-25 00:20:57 +0200

    Implementazione DELETE players in Unity

--- UnityProject/Assets/Scripts/PlayerM/ElencoPlayer.cs
+++ UnityProject/Assets/Scripts/PlayerM/ElencoPlayer.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 using System;
@@ -12,19 +11,39 @@ public class ElencoPlayer : MonoBehaviour
 {
     [SerializeField] private UI_DisplayPlayer uiDiplayer;
 
+    [SerializeField] private GameObject delete_ConfirmTab;
+
+    [SerializeField] private Button  delete_Button;
+    [SerializeField] private Button customize_Button;
     public static string playerSelected;
 
     public void Start()
     {
+        delete_ConfirmTab.SetActive(false);
         Call_GET_Player();
+        customize_Button.interactable = false;
+        delete_Button.interactable = false;
     }
 
+    public void Update()
+    {
 
-    [ContextMenu("Get Players")]
+        if (playerSelected != null)
+        {
+            customize_Button.interactable = true;
+            delete_Button.interactable = true;
+        }
+    }
+
+    private void Call_DELETE_Player()
+    {
+        StartCoroutine(DELETE_Player());
+    } 
     private void Call_GET_Player()
     {
         StartCoroutine(GET_Player());
     }
+
 
     private IEnumerator GET_Player()
     {
@@ -58,7 +77,7 @@ public class ElencoPlayer : MonoBehaviour
     private IEnumerator DELETE_Player()
     {
 
-        using (UnityWebRequest webRequest = UnityWebRequest.Delete("http://localhost:8161/WebServerAPI/data/players"))
+        using (UnityWebRequest webRequest = UnityWebRequest.Delete("http://localhost:8161/WebServerAPI/data/players/"+playerSelected))
         {
             yield return webRequest.SendWebRequest();
 
@@ -80,6 +99,22 @@ public class ElencoPlayer : MonoBehaviour
         }
     }
 
+
+    public void Button_Delete()
+    {
+        delete_ConfirmTab.SetActive(true);
+    }
+
+    public void Button_ConfirmTab_Cancel()
+    {
+        delete_ConfirmTab.SetActive(false);
+    }
+
+    public void Button_ConfirmTab_Delete()
+    {
+        Call_DELETE_Player();
+        delete_ConfirmTab.SetActive(false);
+    }
     //private IEnumerator PUT_ResetPassword_Player()
     //{
 
